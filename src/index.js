16021d1a0ff3c6fb607cbbf68531e7ed09a4fefd
Simran-Dhyani <dhyanisimran7@gmail.com>
2026-03-28 18:22:25 +0530

    utils

--- src/index.js
+++ src/index.js
@@ -2,3 +2,16 @@ import dotenv from "dotenv"
 import connectDB from "./db/index.js"
 dotenv.config()
 connectDB()
+.then(()=>{
+    app.on("error",(e)=>{
+        console.log(e);
+        throw e;
+
+    })
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server running on port:${process.env.PORT}`);
+    })
+})
+.catch((e)=>{
+    console.log("connection failes",e);
+})
