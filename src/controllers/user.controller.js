6ead579629f0222a3773a2a02af9f54bf6a15e07
Simran-Dhyani <dhyanisimran7@gmail.com>
2026-04-12 23:11:48 +0530

    add getWatchHistory endpoint

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import {User} from "../models/user.model.js"
 import { uploadOnCloudinary } from "../utils/cloudinary.js"
 import { ApiResponse } from "../utils/ApiResponse.js"
 import jwt from "jsonwebtoken"
+import mongoose from "mongoose"
 
 const generateAccessAndRefreshTokens=async (userId)=>
 {
@@ -341,5 +342,51 @@ const getUserChannelProfile=asyncHandler(async(req,res)=>{
 }
 )
 
+//watch history
+const getWatchHistory=asyncHandler(async(req,res)=>{
+const user=await User.aggregate([
+    {$match:{
+        _id:new mongoose.Types.ObjectId(req.user._id)
+    }},{
+        $lookup:{
+            from:"videos",
+            localField:"watchHistory",
+            foreignField:"_id",
+            as:"watchHistory",
+            pipeline:[
+                {
+                    $lookup:{
+                        from:"users",
+                        localField:"owner",
+                        foreignField:"_id",
+                        as:"owner",
+                        pipeline:[
+                            {
+                                $project:{
+                                    fullname:1,
+                                    username:1,
+                                    avatar:1
+                                }
+                            }
+                        ]
+                    }
+                },{
+                    $addFields:{
+                        owner:{
+                            $first:"$owner"
+                        }
+                    }
+                }
+            ]
+        }
+    }
+])
+return res.status(200)
+.json(200,user[0].watchHistory,"Watch history fetched successfully !!")
 
-export {registerUser,loginUser,logoutUser,refreshAccessToken,getCurrentUser,changePassword,updateAccountDetails,updateAvatar,updateCoverImage,getUserChannelProfile }
+})
+
+
+export {registerUser,loginUser,logoutUser,refreshAccessToken,getCurrentUser,
+    changePassword,updateAccountDetails,updateAvatar,updateCoverImage,getUserChannelProfile,getWatchHistory
+ }
