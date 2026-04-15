fdc390b2eca71e1f5de58e231c3ce7a215638972
Simran-Dhyani <dhyanisimran7@gmail.com>
2026-04-15 19:48:22 +0530

    setup react vite

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  console.log(import.meta.env.VITE_API_URL)
+
+  return (
+    <>
+     <h1 className="text-red-500 mx-4 my-4 ">Hello</h1>
+    </>
+  )
+}
+
+export default App
